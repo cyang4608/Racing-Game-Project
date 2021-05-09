548366cb1c0f10fc50b5e399be4b2cae30aa5ef8
corbinjstyles <51765978+corbinjstyles@users.noreply.github.com>
2021-05-09 16:18:33 -0500

    main changes

--- TestGLProj/main.cpp
+++ TestGLProj/main.cpp
@@ -17,6 +17,10 @@
 Shader shader; // loads our vertex and fragment shaders
 Shader shader2;
 Shader shader3;
+Shader shader4;
+Shader shader5;
+Shader shader6;
+Shader shader7;
 Model* car;
 Model* wheel1;
 Model* platform1;
@@ -30,7 +34,39 @@ Model* platform8;
 Model* platform9;
 Model* platform10;
 Model* platform11;
+Model* bleachers;
 Model* startingLine;
+Model* field;
+Model* tree;
+Model* tree2;
+Model* tree3;
+Model* tree4;
+Model* tree5;
+Model* tree6;
+Model* racebanner;
+Model* line;
+Model* line2;
+Model* line3;
+Model* line4;
+Model* line5;
+Model* line6;
+Model* line7;
+Model* line8;
+Model* line9;
+Model* line10;
+Model* line11;
+Model* line12;
+Model* line13;
+Model* line14;
+Model* building;
+Model* tube;
+Model* arrow;
+Model* billboard;
+Model* ring;
+Model* billboard2;
+Model* text;
+Model* text2;
+Model* pressbox;
 float rot;
 bool thirdPerson = false;
 bool alt = true;
@@ -83,6 +119,25 @@ void initShader(void)
 	shader3.AddAttribute("vertexPosition");
 	shader3.AddAttribute("vertexNormal");
 
+	//yellow shading
+	shader4.InitializeFromFile("shaders/phong.vert", "shaders/yellow.frag");
+	shader4.AddAttribute("vertexPosition");
+	shader4.AddAttribute("vertexNormal");
+	
+	//red
+	shader5.InitializeFromFile("shaders/phong.vert", "shaders/red.frag");
+	shader5.AddAttribute("vertexPosition");
+	shader5.AddAttribute("vertexNormal");
+	checkError("initShader");
+
+	shader6.InitializeFromFile("shaders/phong.vert", "shaders/blue.frag");
+	shader6.AddAttribute("vertexPosition");
+	shader6.AddAttribute("vertexNormal");
+	checkError("initShader");
+
+	shader7.InitializeFromFile("shaders/phong.vert", "shaders/brown.frag");
+	shader7.AddAttribute("vertexPosition");
+	shader7.AddAttribute("vertexNormal");
 	checkError("initShader");
 }
 
@@ -135,6 +190,15 @@ void display(void)
 	car->render(view * carTrans * glm::translate(0.0f, -2.0f, 00.0f) * glm::scale(1.5f, 2.5f, 1.0f), projection);
 
 	startingLine->render(view * model2 * glm::translate(0.0f, -4.0f, -6.0f) * glm::scale(5.0f, 10.0f, 1.0f), projection);
+	bleachers->render(view * model2 * glm::translate(13.0f, -4.0f, 6.0f) * glm::rotate(90.0f,0.0f,1.0f,0.0f), projection);
+	pressbox->render(view * model2 * glm::translate(-33.0f, -4.0f, 6.0f) * glm::scale(2.8f, 2.8f, 2.8f), projection);
+	tree->render(view * model2 * glm::translate(-20.0f, -4.0f, 40.0f) *glm::scale(1.5f,1.5f,1.5f), projection);
+	tree2->render(view * model2 * glm::translate(-35.0f, -4.0f, 40.0f) * glm::scale(1.5f, 1.5f, 1.5f), projection);
+	tree3->render(view * model2 * glm::translate(-20.0f, -4.0f, 60.0f) * glm::scale(1.5f, 1.5f, 1.5f), projection);
+	tree4->render(view * model2 * glm::translate(-35.0f, -4.0f, 60.0f) * glm::scale(1.5f, 1.5f, 1.5f), projection);
+	tree5->render(view * model2 * glm::translate(-50.0f, -4.0f, 40.0f) * glm::scale(1.5f, 1.5f, 1.5f), projection);
+	tree6->render(view * model2 * glm::translate(-50.0f, -4.0f, 60.0f) * glm::scale(1.5f, 1.5f, 1.5f), projection);
+	field->render(view * model2 * glm::translate(0.0f, -5.0f, -7.0f) * glm::scale(300.0f, 1.0f, 200.0f), projection);
 	platform1->render(view * model2 * glm::translate(0.0f, -4.0f, 20.0f) * glm::scale(5.0f, 10.0f, 25.0f), projection);
 	platform2->render(view * model2 * glm::translate(0.0f, -4.0f, -32.0f) * glm::scale(5.0f, 10.0f, 25.0f), projection);
 	platform3->render(view * model2 * glm::translate(-45.0f, -4.0f, -55.0f) * glm::scale(50.0f, 10.0f, 5.0f), projection);
@@ -146,9 +210,30 @@ void display(void)
 	platform9->render(view * model2 * glm::translate(-160.0f, -4.0f, 90.0f) * glm::scale(75.0f, 10.0f, 5.0f), projection);
 	platform10->render(view * model2 * glm::translate(-90.0f, -4.0f, 70.0f) * glm::scale(5.0f, 10.0f, 25.0f), projection);
 	platform11->render(view * model2 * glm::translate(-40.0f, -4.0f, 50.0f) * glm::scale(45.0f, 10.0f, 5.0f), projection);
-
+	racebanner->render(view * model2 * glm::translate(5.5f, 0.0f, -5.0f) * glm::rotate(90.0f,0.0f,1.0f,0.0f)* glm::scale(1.2f, 1.2f, 1.2f), projection);
+	line->render(view * model2 * glm::translate(0.0f, -3.9f, -16.0f) * glm::rotate(90.0f,0.0f,1.0f,0.0f) * glm::scale(2.0f,2.0f,2.0f), projection);
+	line2->render(view * model2 * glm::translate(-40.0f, -3.9f, -55.0f) * glm::rotate(180.0f, 0.0f, 1.0f, 0.0f) * glm::scale(2.0f, 2.0f, 2.0f), projection);
+	line3->render(view * model2 * glm::translate(-200.0f, -3.9f, -55.0f) * glm::rotate(180.0f, 0.0f, 1.0f, 0.0f) * glm::scale(2.0f, 2.0f, 2.0f), projection);
+	line4->render(view * model2 * glm::translate(-100.0f, -3.9f, -65.0f) * glm::rotate(90.0f, 0.0f, 1.0f, 0.0f) * glm::scale(2.0f, 2.0f, 2.0f), projection);
+	line5->render(view * model2 * glm::translate(-140.0f, -3.9f, -65.0f) * glm::rotate(90.0f, 0.0f, 1.0f, 0.0f) * glm::scale(2.0f, 2.0f, 2.0f), projection);
+	line6->render(view * model2 * glm::translate(-200.0f, -3.9f, 90.0f) * glm::rotate(0.0f, 0.0f, 1.0f, 0.0f) * glm::scale(2.0f, 2.0f, 2.0f), projection);
+	line7->render(view * model2 * glm::translate(-140.0f, -3.9f, 90.0f) * glm::rotate(0.0f, 0.0f, 1.0f, 0.0f) * glm::scale(2.0f, 2.0f, 2.0f), projection);
+	line8->render(view * model2 * glm::translate(-235.0f, -3.9f, 70.0f) * glm::rotate(90.0f, 0.0f, 1.0f, 0.0f) * glm::scale(2.0f, 2.0f, 2.0f), projection);
+	line9->render(view * model2 * glm::translate(-235.0f, -3.9f, -10.0f) * glm::rotate(90.0f, 0.0f, 1.0f, 0.0f) * glm::scale(2.0f, 2.0f, 2.0f), projection);
+	line10->render(view * model2 * glm::translate(-132.0f, -3.9f, -100.0f) * glm::rotate(0.0f, 0.0f, 1.0f, 0.0f) * glm::scale(2.0f, 2.0f, 2.0f), projection);
+	line11->render(view * model2 * glm::translate(0.0f, -3.9f, 36.0f) * glm::rotate(90.0f, 0.0f, 1.0f, 0.0f) * glm::scale(2.0f, 2.0f, 2.0f), projection);
+	line12->render(view * model2 * glm::translate(-40.0f, -3.9f, 50.0f) * glm::rotate(0.0f, 0.0f, 1.0f, 0.0f) * glm::scale(2.0f, 2.0f, 2.0f), projection);
+	line13->render(view * model2 * glm::translate(-90.0f, -3.9f, 80.0f) * glm::rotate(90.0f, 0.0f, 1.0f, 0.0f) * glm::scale(2.0f, 2.0f, 2.0f), projection);
+	line14->render(view * model2 * glm::translate(-180.0f, -3.9f, -55.0f) * glm::rotate(0.0f, 0.0f, 1.0f, 0.0f) * glm::scale(2.0f, 2.0f, 2.0f), projection);
 	//wheel1->render(view * glm::translate(0.0f, -2.0f, 0.0f) * glm::scale(1.5f, 2.5f, 1.0f) * glm::rotate(90.0f,0.0f,0.0f, 0.0f), projection);
-
+	building->render(view * model2 * glm::translate(-130.0f, -4.0f, -6.0f) * glm::scale(5.0f,5.0f,5.0f), projection);
+	tube->render(view * model2 * glm::translate(-235.0f, -4.0f, 20.0f) * glm::rotate(90.0f, 0.0f, 1.0f, 0.0f) * glm::scale(8.0f, 8.0f, 8.0f), projection);
+	arrow->render(view * model2 * glm::translate(0.0f, 0.0f,-65.0f) * glm::rotate(90.0f,0.0f,1.0f,0.0f) *glm::rotate(-90.0f, 1.0f,0.0f,0.0f), projection);
+	billboard->render(view * model2 * glm::translate(-125.0f, -4.0f, -55.0f) * glm::scale(5.0f, 5.0f, 5.0f), projection);
+	ring->render(view * model2 * glm::translate(-160.0f, -4.0f, 97.0f) * glm::scale(3.0f, 3.0f, 3.0f), projection);
+	billboard2->render(view * model2 * glm::translate(-65.0f, -4.0f, 87.0f) * glm::rotate(180.0f, 0.0f,1.0f,0.0f) * glm::scale(5.0f, 5.0f, 5.0f), projection);
+	text->render(view * model2 * glm::translate(-125.0f, 2.0f, -55.0f) * glm::scale(5.0f, 5.0f, 5.0f), projection);
+	text2->render(view * model2 * glm::translate(-65.0f, 2.0f, 87.0f) * glm::rotate(180.0f, 0.0f, 1.0f, 0.0f) * glm::scale(5.0f, 5.0f, 5.0f), projection);
 	glutSwapBuffers(); // Swap the buffers.
 	checkError("display");
 }
@@ -259,19 +344,51 @@ int main(int argc, char** argv)
 	glutKeyboardUpFunc(keyboard);
 	//glutIgnoreKeyRepeat;
 	glEnable(GL_DEPTH_TEST);
-	car = new Model(&shader3, "models/dodge-challenger_model.obj", "models/");
+	car = new Model(&shader, "models/dodge-challenger_model.obj", "models/");
 	startingLine = new Model(&shader2, "models/plane.obj", "models/");
-	platform1 = new Model(&shader, "models/plane.obj", "models/");
-	platform2 = new Model(&shader, "models/plane.obj", "models/");
-	platform3 = new Model(&shader, "models/plane.obj", "models/");
-	platform4 = new Model(&shader, "models/plane.obj", "models/");
-	platform5 = new Model(&shader, "models/plane.obj", "models/");
-	platform6 = new Model(&shader, "models/plane.obj", "models/");
-	platform7 = new Model(&shader, "models/plane.obj", "models/");
-	platform8 = new Model(&shader, "models/plane.obj", "models/");
-	platform9 = new Model(&shader, "models/plane.obj", "models/");
-	platform10 = new Model(&shader, "models/plane.obj", "models/");
-	platform11 = new Model(&shader, "models/plane.obj", "models/");
+	platform1 = new Model(&shader3, "models/plane.obj", "models/");
+	platform2 = new Model(&shader3, "models/plane.obj", "models/");
+	platform3 = new Model(&shader3, "models/plane.obj", "models/");
+	platform4 = new Model(&shader3, "models/plane.obj", "models/");
+	platform5 = new Model(&shader3, "models/plane.obj", "models/");
+	platform6 = new Model(&shader3, "models/plane.obj", "models/");
+	platform7 = new Model(&shader3, "models/plane.obj", "models/");
+	platform8 = new Model(&shader3, "models/plane.obj", "models/");
+	platform9 = new Model(&shader3, "models/plane.obj", "models/");
+	platform10 = new Model(&shader3, "models/plane.obj", "models/");
+	platform11 = new Model(&shader3, "models/plane.obj", "models/");
+	bleachers = new Model(&shader7, "models/bleachers.obj", "models/");
+	pressbox = new Model(&shader7, "models/pressbox.obj", "models/");
+	field = new Model(&shader2, "models/plane.obj", "models/");
+	tree = new Model(&shader2, "models/tree.obj", "models/");
+	tree2 = new Model(&shader2, "models/tree.obj", "models/");
+	tree3 = new Model(&shader2, "models/tree.obj", "models/");
+	tree4 = new Model(&shader2, "models/tree.obj", "models/");
+	tree5 = new Model(&shader2, "models/tree.obj", "models/");
+	tree6 = new Model(&shader2, "models/tree.obj", "models/");
+	racebanner = new Model(&shader, "models/racebanner.obj", "models/");
+	line = new Model(&shader4, "models/line.obj", "models/");
+	line2 = new Model(&shader4, "models/line.obj", "models/");
+	line3 = new Model(&shader4, "models/line.obj", "models/");
+	line4 = new Model(&shader4, "models/line.obj", "models/");
+	line5 = new Model(&shader4, "models/line.obj", "models/");
+	line6 = new Model(&shader4, "models/line.obj", "models/");
+	line7 = new Model(&shader4, "models/line.obj", "models/");
+	line8 = new Model(&shader4, "models/line.obj", "models/");
+	line9 = new Model(&shader4, "models/line.obj", "models/");
+	line10 = new Model(&shader4, "models/line.obj", "models/");
+	line11 = new Model(&shader4, "models/line.obj", "models/");
+	line12 = new Model(&shader4, "models/line.obj", "models/");
+	line13 = new Model(&shader4, "models/line.obj", "models/");
+	line14 = new Model(&shader4, "models/line.obj", "models/");
+	building = new Model(&shader6, "models/building.obj", "models/");
+	tube = new Model(&shader5, "models/tube.obj", "models/");
+	arrow = new Model(&shader5, "models/arrow.obj", "models/");
+	billboard = new Model(&shader4, "models/billboard.obj", "models/");
+	ring = new Model(&shader4, "models/ring.obj", "models/");
+	billboard2 = new Model(&shader4, "models/billboard.obj", "models/");
+	text = new Model(&shader3, "models/text.obj", "models/");
+	text2 = new Model(&shader3, "models/text2.obj", "models/");
 	//wheel1 = new Model(&shader, "models/wheel1.obj", "models/");
 	glutMainLoop();
 
